ac0e0d0fda6a9bfb321be803e6a03db752a2cbe7
Izzu2000 <sarahprog667@gmail.com>
2021-06-16 18:06:09 +0800

    Created a rough sketch of training GUI tab

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter.ttk import Notebook
 import json
 import contextlib
 import numpy as np
 import os
+import io
 import tensorflow as tf
+import sys
+from PIL import Image, ImageTk
 from tensorflow import keras
 from tensorflow.keras import layers, models
 
@@ -13,7 +17,21 @@ SETTINGS_PATH = 'settings.json'
 PLANET_PATH_KEY = "planet_folder_path"
 TRAINING_PATH_KEY = 'training_history_path'
 CLASS_KEY = 'classes_saved'
+BACKGROUND_HEX = '#acacac'
 IMAGE_WIDTH, IMAGE_HEIGHT = 700, 700
+
+
+class ConsoleWritter(io.IOBase):
+    """This updates from console to the Text object"""
+    def __init__(self, obj):
+        self.obj = obj
+
+    def write(self, arg):
+        text = self.obj
+        text.configure(state='normal')
+        text.insert('end', arg)
+        text.see("end")
+        text.configure(state='disabled')
 
 
 def save(data):
@@ -39,29 +57,29 @@ def load_settings():
     return save({})
 
 
-def load_GUI(**options):
-    window = tk.Tk()
-    top_frame = tk.Frame(master=window, width=20, height=50, bg="red")
-    top_frame.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+def create_button_image(window, *, image, title, description=None, padx=7, text_size=11, **kwargs):
+    font = ('Arial', text_size, 'bold')
+    button_text = '\n'.join((title, description)) if description else title
+    button_border = tk.Frame(window, highlightbackground="#858585",
+                             highlightthickness=2, bd=0)
+    button = tk.Button(
+        button_border,
+        text=button_text,
+        anchor='w',
+        justify='left',
+        font=font,
+        padx=padx,
+        borderwidth=0,
+        **kwargs
+    )
 
-    inner_frame = tk.Frame(master=top_frame, height=40, bg='white')
-    inner_frame.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
-    label1 = tk.Label(master=inner_frame, text="Root Planet Folder Path", bg="white")
-    label1.place(x=5, y=5)
-    inner_frame2 = tk.Frame(master=top_frame, bg='white')
-    inner_frame2.pack(fill=tk.BOTH, side=tk.LEFT)
-
-    planet_path_GUI = tk.Entry(master=inner_frame2, text="")
-    planet_path_GUI.place(x=0, y=0)
-
-    if not (planet_path := options.get(PLANET_PATH_KEY)):
-        planet_path = filedialog.askdirectory()
-        options.update({PLANET_PATH_KEY: planet_path})
-        save(options)
-
-    text_box = tk.Text()
-    text_box.pack()
-    window.mainloop()
+    with Image.open(image).convert('RGBA') as img_loaded:
+        img_loaded = img_loaded.resize((50, 50))
+        photo = ImageTk.PhotoImage(img_loaded)
+        button.image = photo
+        button.config(image=photo, compound=tk.LEFT)
+    button.pack()
+    return button_border
 
 
 def create_or_load_model(objects, **settings):
@@ -138,7 +156,7 @@ def data_train(planet_path, epochs=200):
 
     settings = save_key(CLASS_KEY, train_ds.class_names)
 
-    print("classes:", train_ds.class_names)
+    print("Identified Classes:", ", ".join(train_ds.class_names))
     # Normalization
     # changes from [0, 255] into [0, 1] ranges,
     normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
@@ -154,9 +172,11 @@ def data_train(planet_path, epochs=200):
 
     # trains it with whatever epoch given
     model.fit(train_ds, validation_data=valid_ds, epochs=epochs)
-
+    print("Training ended...")
+    path = settings.get(TRAINING_PATH_KEY) or "current directory"
+    print("Saving data to", path)
     # saves the model
-    model.save(settings.get(TRAINING_PATH_KEY))
+    model.save(path)
 
 
 def test_data(path, label, **data):
@@ -216,19 +236,120 @@ def test_folder_data(root):
     print("Final Accuracy:", sum(results) / len(results) * 100)
 
 
+def load_recognition_GUI(tab_layout, **settings):
+    """Recognition Tab GUI"""
+    tab_recognition = tk.Frame(tab_layout)
+    tab_recognition.pack(fill='both')
+    label = tk.Label(tab_recognition, text="Recognition")
+    label.pack()
+    tab_recognition.pack(fill='both')
+    return tab_recognition
+
+
+def load_training_GUI(tab_layout, **settings):
+    """Training Tab GUI"""
+    padding = {'padx': 7, 'pady': 7}
+    tab_training = tk.Frame(tab_layout, bg=BACKGROUND_HEX)
+    tab_training.grid_columnconfigure(0, weight=1, uniform="fred")
+    tab_training.pack(fill='both')
+
+    text_box = tk.Text(tab_training)
+    sys.stdout = ConsoleWritter(text_box)
+    if planet_path := settings.get(PLANET_PATH_KEY):
+        print("Training Path:", planet_path)
+    if save_path := settings.get(TRAINING_PATH_KEY):
+        print("Save Path:", save_path)
+
+    # When training folder button is clicked
+    def training_folder():
+        nonlocal planet_path
+        planet_path = filedialog.askdirectory(initialdir=planet_path)
+        print("Training Path:", planet_path)
+        settings.update(save_key(PLANET_PATH_KEY, planet_path))
+
+    # When save folder button is clicked
+    def save_path_folder():
+        nonlocal save_path
+        save_path = filedialog.askdirectory(initialdir=save_path)
+        print("Save Path:", save_path)
+        settings.update(save_key(TRAINING_PATH_KEY, save_path))
+
+    # When running folder button is clicked
+    def running_train():
+        if planet_path is None:
+            print("Planets path folder is not selected. Please select a path.")
+        elif save_path is None:
+            print("Saving Path is not selected. Please select a path.")
+        else:
+            data_train(planet_path)
+
+    # Create buttons
+    place = create_button_image(
+        tab_training,
+        image=r'Resources\Folder_Icon.png',
+        title="Planets",
+        description="Folder that contains all images for training.",
+        bg='#dfdfdf',
+        command=training_folder
+    )
+    place.grid(row=1, column=0, **padding)
+
+    place = create_button_image(
+        tab_training,
+        image=r'Resources\Folder_Icon.png',
+        title="Save Model",
+        description="Path to save the AI model.",
+        bg='#dfdfdf',
+        command=save_path_folder
+    )
+    place.grid(row=1, column=1, **padding)
+    text_box.grid(row=2, column=0, columnspan=2, **padding)
+    place = create_button_image(
+        tab_training,
+        text_size=10,
+        image=r'Resources\Start_icon.png',
+        title="Start Training",
+        bg='#dfdfdf',
+        command=running_train
+    )
+    place.grid(row=3, column=1, sticky='e', **padding)
+    return tab_training
+
+
+def load_GUI(**options):
+    window = tk.Tk()
+    window.title("Planet Recognition")
+
+    # Tab Layout
+    tab_frame = tk.Frame(window, bg=BACKGROUND_HEX)
+    tab_frame.pack(fill='both')
+    tab_layout = Notebook(tab_frame)
+
+    # Create tabs
+    tab_recognition = load_recognition_GUI(tab_layout, **options)
+    tab_training = load_training_GUI(tab_layout, **options)
+
+    # Add the created tab
+    tab_layout.add(tab_recognition, text="Recognition")
+    tab_layout.add(tab_training, text="AI Training")
+
+    # Starts
+    tab_layout.pack(fill='both')
+    window.mainloop()
+
+
 if __name__ == '__main__':
     settings = load_settings()
-    # load_GUI(**settings) do this once gui is made
-
+    load_GUI(**settings)
     # Asks for training path
-    training_path = r"C:\Users\sarah\PycharmProjects\Planet-Recognition\Planet"
-    # Train the model
-    data_train(training_path, epochs=2)
+    # training_path = r"C:\Users\sarah\PycharmProjects\Planet-Recognition\Planet"
+    # # Train the model
+    # data_train(r"C:\Users\sarah\PycharmProjects\Planet-Recognition\Planet", epochs=2)
 
-    # Do this if you wanna check every test data there are
-    root = r'C:\Users\sarah\PycharmProjects\Planet-Recognition\Test Images'
-    test_folder_data(root)
-
-    # # Do this if you want to test 1 data
-    path = r"C:\Users\sarah\PycharmProjects\Planet-Recognition\Test Images\Earth 2.jpg"
-    test_single_data(path)
+    # # Do this if you wanna check every test data there are
+    # root = r'C:\Users\sarah\PycharmProjects\Planet-Recognition\Test Images'
+    # test_folder_data(root)
+    #
+    # # # Do this if you want to test 1 data
+    # path = r"C:\Users\sarah\PycharmProjects\Planet-Recognition\Test Images\Earth 2.jpg"
+    # test_single_data(path)
