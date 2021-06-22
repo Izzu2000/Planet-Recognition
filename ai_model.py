907bca2fde3dd65c9794835b3b06e0cdca2b856b
Izzu2000 <sarahprog667@gmail.com>
2021-06-22 14:49:05 +0800

    Susun coding, ai_model.py contains training/related to models, main.py is related to GUI

---
+++ ai_model.py
@@ -0,0 +1,185 @@
+import threading
+import numpy as np
+import json
+import contextlib
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, models
+
+# Constants
+SETTINGS_PATH = 'settings.json'
+PLANET_PATH_KEY = "planet_folder_path"
+TRAINING_PATH_KEY = 'training_history_path'
+CLASS_KEY = 'classes_saved'
+BACKGROUND_HEX = '#acacac'
+IMAGE_WIDTH, IMAGE_HEIGHT = 700, 700
+
+
+def save(data):
+    with open(SETTINGS_PATH, 'w+') as j:
+        json.dump(data, j, indent=4)
+    return data
+
+
+def load():
+    with open(SETTINGS_PATH, 'r') as j:
+        return json.load(j)
+
+
+def save_key(key, value):
+    s = load()
+    s.update({key: value})
+    return save(s)
+
+
+def load_settings():
+    with contextlib.suppress(IOError):
+        return load()
+    return save({})
+
+
+def run_in_thread(func):
+    """Run the decorated function in a thread, and directly run when blocking is set to True"""
+    def runner(*args, blocking=False, **kwargs):
+        if not blocking:
+            thread = threading.Thread(target=func, args=args, kwargs=kwargs)
+            thread.start()
+            return thread
+        return func(*args, **kwargs)
+    return runner
+
+
+def create_or_load_model(objects, settings):
+    if not (path := settings.get(TRAINING_PATH_KEY)):
+        ACTIVATION_FUNC = 'relu'
+
+        # Input shape is (IMAGE_HEIGHT, IMAGE_WIDTH, 3)
+        # - reduce computational time if image size is lower
+        # - introduces the same resolution for all images input
+        # 3 refers to the channels
+
+        # Using max pooling
+        # - to focus on the planet itself
+        # - ignore the dark background of space
+
+        # activation: relu
+        # - to extract feature from planets
+
+        # Flatten is the conversion to be inputted into the node
+
+        # it contain 128 nodes
+        # Final dense is for the 5 objects classified
+        model = models.Sequential([
+            layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)),
+            layers.Conv2D(16, 3, padding='same', activation=ACTIVATION_FUNC),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding='same', activation=ACTIVATION_FUNC),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding='same', activation=ACTIVATION_FUNC),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation=ACTIVATION_FUNC),
+            layers.Dense(objects)
+        ])
+
+        # Adam is the best among the adaptive optimizers for most cases
+        # -combinations of Adadelta and RMSprop, recommended to use adam for most cases
+
+        # SparseCategoricalCrossentropy pretty much does computes the loss
+
+        # metrics just shows our accuracy
+        model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy']
+        )
+        # Saving model
+        path = 'training_history'
+        settings.update(save_key(TRAINING_PATH_KEY, path))
+        return model
+
+    return keras.models.load_model(path)
+
+
+@run_in_thread
+def data_train(planet_path, epochs=200):
+    BATCH_SIZE = 5
+    # gets all the images from a root folder, Planet -> Jupiter, Mars, ...
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        planet_path,
+        subset='training',
+        seed=500,
+        validation_split=0.1,
+        image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+        batch_size=BATCH_SIZE
+    )
+    valid_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        planet_path,
+        subset='validation',
+        seed=500,
+        validation_split=0.5,
+        image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+        batch_size=BATCH_SIZE
+    )
+
+    settings = save_key(CLASS_KEY, train_ds.class_names)
+
+    print("Identified Classes:", ", ".join(train_ds.class_names))
+    # Normalization
+    # changes from [0, 255] into [0, 1] ranges,
+    normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
+
+    def func(x, y):
+        print("Normalizing")
+        return normalization_layer(x), y
+
+    train_ds.map(func)
+
+    # Get/create model
+    model = create_or_load_model(len(train_ds.class_names), settings)
+
+    # trains it with whatever epoch given
+    model.fit(train_ds, validation_data=valid_ds, epochs=epochs)
+    print("Training ended...")
+    path = settings.get(TRAINING_PATH_KEY) or "current directory"
+    print("Saving data to", path)
+    # saves the model
+    model.save(path)
+
+
+def test_data(path, label, data):
+    # Loads the image, and resize to the target_size
+    img = keras.preprocessing.image.load_img(
+        path, target_size=(IMAGE_HEIGHT, IMAGE_WIDTH)
+    )
+
+    # converts PIL image into numpy array
+    # image -> [[r, g, b], ..., [r, g, b]] array
+    img_array = keras.preprocessing.image.img_to_array(img)
+
+    # [r, g, b] -> [a, r, g, b]
+    img_array = tf.expand_dims(img_array, 0)
+
+    # gets the saved model, predict and get the score
+    model = keras.models.load_model(data.get(TRAINING_PATH_KEY))
+    predictions = model.predict(img_array)
+    # returns a list of scores for all 5 nodes
+    score = tf.nn.softmax(predictions[0])
+
+    # get all class as a list
+    classes = data[CLASS_KEY]
+    # np.argmax gets the index of the highest value
+    # np.max gets the highest value of the list
+    predicted = classes[np.argmax(score)]
+    score = 100 * np.max(score)
+    print("Predicted Planet:", predicted, score)
+    print("Actual Planet:", label)
+    return predicted, score, label
+
+
+def test_single_data(path, settings):
+    path = path.replace("/", "\\")
+    x = path.split('\\')[-1]
+    y = x.split()[0]
+    z = y.split('.')[0]
+    return test_data(path, z.capitalize(), settings)
--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter.ttk import Notebook
-import threading
-import json
-import contextlib
-import numpy as np
-import os
 import io
-import tensorflow as tf
 import sys
+import ai_model
+from ai_model import BACKGROUND_HEX, PLANET_PATH_KEY, TRAINING_PATH_KEY
 from PIL import Image, ImageTk
-from tensorflow import keras
-from tensorflow.keras import layers, models
 from tensorflow.python.keras.preprocessing import dataset_utils
 from tensorflow.python.keras.preprocessing.image_dataset import ALLOWLIST_FORMATS
-
-# Constants
-# hello
-SETTINGS_PATH = 'settings.json'
-PLANET_PATH_KEY = "planet_folder_path"
-TRAINING_PATH_KEY = 'training_history_path'
-CLASS_KEY = 'classes_saved'
-BACKGROUND_HEX = '#acacac'
-IMAGE_WIDTH, IMAGE_HEIGHT = 700, 700
 
 
 class ConsoleWritter(io.IOBase):
@@ -41,40 +26,6 @@ class ConsoleWritter(io.IOBase):
         text.insert('end', arg)
         text.see("end")
         text.configure(state='disabled')
-
-
-def run_in_thread(func):
-    """Run the decorated function in a thread, and directly run when blocking is set to True"""
-    def runner(*args, blocking=False, **kwargs):
-        if not blocking:
-            thread = threading.Thread(target=func, args=args, kwargs=kwargs)
-            thread.start()
-            return thread
-        return func(*args, **kwargs)
-    return runner
-
-
-def save(data):
-    with open(SETTINGS_PATH, 'w+') as j:
-        json.dump(data, j, indent=4)
-    return data
-
-
-def load():
-    with open(SETTINGS_PATH, 'r') as j:
-        return json.load(j)
-
-
-def save_key(key, value):
-    s = load()
-    s.update({key: value})
-    return save(s)
-
-
-def load_settings():
-    with contextlib.suppress(IOError):
-        return load()
-    return save({})
 
 
 def create_button_image(window, *, image, title, description=None, padx=7, text_size=11, **kwargs):
@@ -102,162 +53,7 @@ def create_button_image(window, *, image, title, description=None, padx=7, text_
     return button_border
 
 
-def create_or_load_model(objects, **settings):
-    if not (path := settings.get(TRAINING_PATH_KEY)):
-        ACTIVATION_FUNC = 'relu'
-
-        # Input shape is (IMAGE_HEIGHT, IMAGE_WIDTH, 3)
-        # - reduce computational time if image size is lower
-        # - introduces the same resolution for all images input
-        # 3 refers to the channels
-
-        # Using max pooling
-        # - to focus on the planet itself
-        # - ignore the dark background of space
-
-        # activation: relu
-        # - to extract feature from planets
-
-        # Flatten is the conversion to be inputted into the node
-
-        # it contain 128 nodes
-        # Final dense is for the 5 objects classified
-        model = models.Sequential([
-            layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)),
-            layers.Conv2D(16, 3, padding='same', activation=ACTIVATION_FUNC),
-            layers.MaxPooling2D(),
-            layers.Conv2D(32, 3, padding='same', activation=ACTIVATION_FUNC),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, 3, padding='same', activation=ACTIVATION_FUNC),
-            layers.MaxPooling2D(),
-            layers.Flatten(),
-            layers.Dense(128, activation=ACTIVATION_FUNC),
-            layers.Dense(objects)
-        ])
-
-        # Adam is the best among the adaptive optimizers for most cases
-        # -combinations of Adadelta and RMSprop, recommended to use adam for most cases
-
-        # SparseCategoricalCrossentropy pretty much does computes the loss
-
-        # metrics just shows our accuracy
-        model.compile(
-            optimizer='adam',
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            metrics=['accuracy']
-        )
-        # Saving model
-        path = 'training_history'
-        settings.update(save_key(TRAINING_PATH_KEY, path))
-        return model
-
-    return keras.models.load_model(path)
-
-
-@run_in_thread
-def data_train(planet_path, epochs=200):
-    BATCH_SIZE = 5
-    # gets all the images from a root folder, Planet -> Jupiter, Mars, ...
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        planet_path,
-        subset='training',
-        seed=500,
-        validation_split=0.1,
-        image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
-        batch_size=BATCH_SIZE
-    )
-    valid_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        planet_path,
-        subset='validation',
-        seed=500,
-        validation_split=0.5,
-        image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
-        batch_size=BATCH_SIZE
-    )
-
-    settings = save_key(CLASS_KEY, train_ds.class_names)
-
-    print("Identified Classes:", ", ".join(train_ds.class_names))
-    # Normalization
-    # changes from [0, 255] into [0, 1] ranges,
-    normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
-
-    def func(x, y):
-        print("Normalizing")
-        return normalization_layer(x), y
-
-    train_ds.map(func)
-
-    # Get/create model
-    model = create_or_load_model(len(train_ds.class_names))
-
-    # trains it with whatever epoch given
-    model.fit(train_ds, validation_data=valid_ds, epochs=epochs)
-    print("Training ended...")
-    path = settings.get(TRAINING_PATH_KEY) or "current directory"
-    print("Saving data to", path)
-    # saves the model
-    model.save(path)
-
-
-def test_data(path, label, **data):
-    # Loads the image, and resize to the target_size
-    img = keras.preprocessing.image.load_img(
-        path, target_size=(IMAGE_HEIGHT, IMAGE_WIDTH)
-    )
-
-    # converts PIL image into numpy array
-    # image -> [[r, g, b], ..., [r, g, b]] array
-    img_array = keras.preprocessing.image.img_to_array(img)
-
-    # [r, g, b] -> [a, r, g, b]
-    img_array = tf.expand_dims(img_array, 0)
-
-    # gets the saved model, predict and get the score
-    model = keras.models.load_model(data.get(TRAINING_PATH_KEY))
-    predictions = model.predict(img_array)
-    # returns a list of scores for all 5 nodes
-    score = tf.nn.softmax(predictions[0])
-
-    # get all class as a list
-    classes = data[CLASS_KEY]
-    # np.argmax gets the index of the highest value
-    # np.max gets the highest value of the list
-    predicted = classes[np.argmax(score)]
-    score = 100 * np.max(score)
-    print("Predicted Planet:", predicted, score)
-    print("Actual Planet:", label)
-    return predicted, score, label
-
-
-def get_training_folder(ask=False, **options):
-    if not (planet_path := options.get(PLANET_PATH_KEY) and not ask):
-        planet_path = filedialog.askdirectory()
-        options.update({PLANET_PATH_KEY: planet_path})
-        save(options)
-    print("Training Path:", planet_path)
-    return planet_path
-
-
-def test_single_data(path):
-    path = path.replace("/", "\\")
-    x = path.split('\\')[-1]
-    y = x.split()[0]
-    z = y.split('.')[0]
-    return test_data(path, z.capitalize(), **settings)
-
-
-def test_folder_data(root):
-    def predicting(x):
-        predicted, _, label = test_single_data(f"{root}/{x}")
-        print()
-        return predicted == label
-
-    results = [*map(predicting, os.listdir(root))]
-    print("Final Accuracy:", sum(results) / len(results) * 100)
-
-
-def load_recognition_GUI(tab_layout, **settings):
+def load_recognition_gui(tab_layout, settings):
     """Recognition Tab GUI"""
     tab_recognition = tk.Frame(tab_layout)
     tab_recognition.pack(fill='both')
@@ -267,7 +63,7 @@ def load_recognition_GUI(tab_layout, **settings):
     return tab_recognition
 
 
-def load_training_GUI(tab_layout, **settings):
+def load_training_gui(tab_layout, settings):
     """Training Tab GUI"""
     padding = {'padx': 7, 'pady': 7}
     tab_training = tk.Frame(tab_layout, bg=BACKGROUND_HEX)
@@ -287,20 +83,20 @@ def load_training_GUI(tab_layout, **settings):
         planet_path = filedialog.askdirectory(initialdir=planet_path)
         print("Training Path:", planet_path)
 
-        @run_in_thread
+        @ai_model.run_in_thread
         def find_classes():
             # shows expected class_name, this may be blocking heavily
             _, _, class_names = dataset_utils.index_directory(planet_path, 'inferred', ALLOWLIST_FORMATS)
             print("Classes found:", ", ".join(class_names))
         find_classes()
-        settings.update(save_key(PLANET_PATH_KEY, planet_path))
+        settings.update(ai_model.save_key(PLANET_PATH_KEY, planet_path))
 
     # When save folder button is clicked
     def save_path_folder():
         nonlocal save_path
         save_path = filedialog.askdirectory(initialdir=save_path)
         print("Save Path:", save_path)
-        settings.update(save_key(TRAINING_PATH_KEY, save_path))
+        settings.update(ai_model.save_key(TRAINING_PATH_KEY, save_path))
 
     # When running folder button is clicked
     def running_train():
@@ -309,7 +105,7 @@ def load_training_GUI(tab_layout, **settings):
         elif save_path is None:
             print("Saving Path is not selected. Please select a path.")
         else:
-            data_train(planet_path)
+            ai_model.data_train(planet_path)
 
     # Create buttons
     place = create_button_image(
@@ -344,7 +140,7 @@ def load_training_GUI(tab_layout, **settings):
     return tab_training
 
 
-def load_GUI(**options):
+def load_gui(options):
     window = tk.Tk()
     window.title("Planet Recognition")
 
@@ -354,8 +150,8 @@ def load_GUI(**options):
     tab_layout = Notebook(tab_frame)
 
     # Create tabs
-    tab_recognition = load_recognition_GUI(tab_layout, **options)
-    tab_training = load_training_GUI(tab_layout, **options)
+    tab_recognition = load_recognition_gui(tab_layout, options)
+    tab_training = load_training_gui(tab_layout, options)
 
     # Add the created tab
     tab_layout.add(tab_recognition, text="Recognition")
@@ -367,8 +163,8 @@ def load_GUI(**options):
 
 
 if __name__ == '__main__':
-    settings = load_settings()
-    load_GUI(**settings)
+    loaded_settings = ai_model.load_settings()
+    load_gui(loaded_settings)
     # Asks for training path
     # training_path = r"C:\Users\sarah\PycharmProjects\Planet-Recognition\Planet"
     # # Train the model
